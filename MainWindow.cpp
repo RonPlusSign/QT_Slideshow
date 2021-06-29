7594f6f8e43f8b1043635e4aeb0049a917dcc6c3
Andrea Delli <andrea.delli2001@gmail.com>
2021-06-29 09:23:56 +0200

    feat: MainWindow GUI & basic logic

--- MainWindow.cpp
+++ MainWindow.cpp
@@ -1,12 +1,33 @@
 #include "MainWindow.h"
 #include "./ui_MainWindow.h"
 
-MainWindow::MainWindow(QWidget *parent)
-        : QMainWindow(parent), ui(new Ui::MainWindow) {
-    ui->setupUi(this);
+MainWindow::MainWindow(QWidget *parent) : QMainWindow(parent), ui(new Ui::MainWindow), imageList(nullptr), controller(nullptr) { ui->setupUi(this); }
+
+MainWindow::MainWindow(SlideshowController *controller, ImageList *imageList, QWidget *parent) : MainWindow(parent) {
+    this->controller = controller;
+    this->imageList = imageList;
+
+    this->imageList->subscribe(this);
 }
 
 MainWindow::~MainWindow() {
     delete ui;
+    this->imageList->unsubscribe(this);
+}
+
+void MainWindow::on_play_clicked() {
+
+}
+
+void MainWindow::on_next_clicked() {
+
+}
+
+void MainWindow::on_previous_clicked() {
+
+}
+
+void MainWindow::update() {
+
 }
 
--- MainWindow.h
+++ MainWindow.h
@@ -2,20 +2,36 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
+#include <interfaces/Observer.h>
+#include <controller/SlideshowController.h>
+#include <model/ImageList.h>
 
 QT_BEGIN_NAMESPACE
 namespace Ui { class MainWindow; }
 QT_END_NAMESPACE
 
-class MainWindow : public QMainWindow {
+class MainWindow : public QMainWindow, public Observer {
 Q_OBJECT
 
 public:
-    MainWindow(QWidget *parent = nullptr);
-    ~MainWindow();
+    MainWindow(SlideshowController *controller, ImageList *imageList, QWidget *parent = nullptr);
+    ~MainWindow() override;
+
+    void update() override;
+
+private slots:
+    void on_play_clicked();
+
+    void on_next_clicked();
+
+    void on_previous_clicked();
 
 private:
+    explicit MainWindow(QWidget *parent);
+
     Ui::MainWindow *ui;
+    SlideshowController *controller;
+    ImageList *imageList;
 };
 
 #endif // MAINWINDOW_H
