c65ff4ce37493c1b785ba55395f299b1742f273a
Andrea Delli <andrea.delli2001@gmail.com>
2021-06-29 09:25:19 +0200

    feat: added files to CMakeLists & opened dialogs from main

--- main.cpp
+++ main.cpp
@@ -1,11 +1,19 @@
 #include "MainWindow.h"
 
 #include <QApplication>
+#include <components/ChooseFilesDialog.h>
 
-int main(int argc, char *argv[])
-{
-    QApplication a(argc, argv);
-    MainWindow w;
-    w.show();
-    return a.exec();
+int main(int argc, char *argv[]) {
+    QApplication app(argc, argv);
+
+    ImageList model;
+
+    QDialog *chooseFilesDialog = new ChooseFilesDialog(&model);
+    chooseFilesDialog->show();
+
+    SlideshowController controller(&model);
+    MainWindow view(&controller, &model);
+
+    view.show();
+    return app.exec();
 }
