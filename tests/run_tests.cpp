b2d4e2c224ea95eb7527dda7bef1ba07f6c2200c
Andrea Delli <andrea.delli2001@gmail.com>
2021-07-03 13:47:36 +0200

    tests: updated CMakeLists files

---
+++ tests/run_tests.cpp
@@ -0,0 +1,10 @@
+//
+// Created by andre on 03/07/21.
+//
+
+#include <gtest/gtest.h>
+
+int main(int argc, char *argv[]) {
+    testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
